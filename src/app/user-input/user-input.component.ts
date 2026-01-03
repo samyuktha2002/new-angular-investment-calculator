d616886f83fa4ed1b5d1affb5700bc84045c67cb
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-01-03 14:38:19 +0530

    Migrating all from standalone to module based components

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,8 @@
-import { Component, output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
