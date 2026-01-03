2a24f7977314af4b014d1c0153441c9d3795013f
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-01-03 13:35:11 +0530

    Using signals in services

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, computed} from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
@@ -12,7 +12,5 @@ import { InvestmentService } from '../investment.service';
 export class InvestmentResultsComponent {
   constructor(private investmentService: InvestmentService){}
 
-  get results(){
-    return this.investmentService.resultsData;
-  }
+results = computed(() => { return this.investmentService.resultsData()});
 }
